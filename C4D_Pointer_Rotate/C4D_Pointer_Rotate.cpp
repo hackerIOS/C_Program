6967f3762701f807156600d210eae0d3a9e777b8
hackerIOS <1009489031@qq.com>
2017-08-30 18:11:49 +0800

    Add files via upload

---
+++ C4D_Pointer_Rotate/C4D_Pointer_Rotate.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+using namespace std;
+int fx=0;//Direction
+int wzx=1;
+int wzy=3;
+
+void sleepfc(){
+	int cc=0;
+	for(;cc!=10000;){
+		cc++;
+	}
+}
+
+
+int main(){
+	for (;;){
+		if (wzx==1 && wzy==3){
+			cout<<endl;
+			cout<<endl;
+			cout<<".";
+			wzx++;
+			wzy=0;
+		}
+		sleepfc();
+		//clrscr();
+		system("cls");
+	}
+	return 0;
+}
+
